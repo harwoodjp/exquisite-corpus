18c18c7089284dd1583be001ac8a98ffa7fad5e1
Justin Harwood <justin.harwood@servicenow.com>
2020-10-15 16:55:32 -0500

    upd for python3

--- app.py
+++ app.py
@@ -3,65 +3,68 @@ import requests
 import json
 from flask import Flask
 
-
-
-def getWords(path):   
-    words = open(path, "rb").readlines()
+def getDict(path):   
     pairs = []
-    for w in words:
+
+    for w in open(path, encoding="ISO-8859-1").readlines():
         pairs.append(w.split("\\"))
+
     words_dict = dict(pairs)
 
     nouns = []
     verbs = []
     adjectives = []
+    adverbs = []
 
-    for w, t in words_dict.iteritems():
+    for w, t in words_dict.items():
         if "N" in t:
             nouns.append(w)
         if "V" in t:
             verbs.append(w)
         if "A" in t:
             adjectives.append(w)
+        if "v" in t:
+            adverbs.append(w)
 
     return {
         "nouns": nouns,
         "verbs": verbs,
-        "adjectives": adjectives
+        "adjectives": adjectives,
+        "adverbs": adverbs
     }
 
-def rw(d, t):
-    if t == "N":
-        return random.choice(d["nouns"])
-    if t == "V":
-        return random.choice(d["verbs"])
-    if t == "A":
-        return random.choice(d["adjectives"])
-
-
-def rws(d, ts):
+def rw(d, ts):
     result = ""
+
     for t in ts:
-        result += rw(d, t) + " "
-    return result[:-1]
+      if t == "N":
+          result += random.choice(d["nouns"]) + ","
+      if t == "V":
+          result += random.choice(d["verbs"]) + ","
+      if t == "A":
+          result += random.choice(d["adjectives"]) + ","
+      if t == "v":
+          result += random.choice(d["adverbs"]) + ","
+
+    return result
 
 def getImage(q):
     url = "https://api.cognitive.microsoft.com/bing/v7.0/images/search?mkt=en-us&q=" + q
-    headers = { "ocp-apim-subscription-key" : "" }
-    print requests.get(url, headers=headers).json()
+    key = "" # from azure portal
+    headers = { "ocp-apim-subscription-key" : key }
     return requests.get(url, headers=headers).json()["value"][0]["thumbnailUrl"]
 
-d = getWords("mobypos.txt")
+d = getDict("mobypos.txt")
+colors = ["#adccff", "#b7fff9", "#c6b5ff", "#ffccfb", "#e8ffcc", "#ffe4cc"]
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    rws2 = [rw(d, "N"), rw(d, "N"), rw(d, "N")]
-    imgs = [ getImage(rws2[0]), getImage(rws2[1]), getImage(rws2[2]) ]
-    colors = ["#adccff", "#b7fff9", "#c6b5ff", "#ffccfb", "#e8ffcc", "#ffe4cc"]
+def main():
+    words = rw(d, "NNN").split(",")
+    imgs = [getImage(words[0]), getImage(words[1]), getImage(words[2])]
 
-    html = """
+    return """
         <html>
             <head>
                 <style>
@@ -69,9 +72,11 @@ def hello_world():
                         background-color: {randomColor}
                     }}
                     .images {{
+                        display: flex;
                         padding: 1em;
                     }}
                     img {{
+                        align-self: flex-start;
                         max-height: 22vw;
                         max-width: 29%;
                         padding: 1em;
@@ -79,7 +84,7 @@ def hello_world():
                 </style>
             </head>
             <body>
-                {rws2[0]} {rws2[1]} {rws2[2]} 
+                {words[0]} {words[1]} {words[2]} 
                 <div class="images">
                     <img src="{img1}"> 
                     <img src="{img2}">
@@ -87,7 +92,4 @@ def hello_world():
                 </div>
             </body>
         </html>
-    """.format(randomColor=random.choice(colors), rws2=rws2, img1=imgs[0], img2=imgs[1], img3=imgs[2])
-
-    return html
-
+    """.format(randomColor=random.choice(colors), words=words, img1=imgs[0], img2=imgs[1], img3=imgs[2])
